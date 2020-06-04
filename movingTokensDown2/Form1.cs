3ecf435b291b0282f209637e1942e1467930a7b9
Andrew Booth <16060@bayfield-high.school.nz>
2020-06-05 10:46:39 +1200

    Add project files.

---
+++ movingTokensDown2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace movingTokensDown2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //35px by 35px is the redToken image...
+            //390px width of form, 20px on each side to fit 10 tokens on screen...
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
